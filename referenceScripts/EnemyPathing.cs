d3b6fdd2c3fcc9b435d9647773fd72e89016ab8d
mzone242 <55608745+mzone242@users.noreply.github.com>
2019-09-21 09:32:24 -0500

    Update EnemyPathing.cs

--- referenceScripts/EnemyPathing.cs
+++ referenceScripts/EnemyPathing.cs
@@ -1,37 +1,46 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyPathing : MonoBehaviour {
+public class EnemyPathing : MonoBehaviour
+{
+    // array of waypoints to move between
+    // waypoints are just empty GameObjects set at desired locations
+    [SerializeField]
+    Transform[] waypoints;
+    // speed of enemy
+    [SerializeField]
+    float moveSpeed = 1f;
+    // start of array
+    int waypointIndex = 0;
 
-	[SerializeField]
-	Transform[] waypoints;
+    void Start()
+    {
+        // sets enemy position to first waypoint
+        transform.position = waypoints[waypointIndex].transform.position;
+    }
 
-	[SerializeField]
-	float moveSpeed = 2f;
+    void Update()
+    {
+        Move();
+    }
 
-	int waypointIndex = 0;
-
-	void Start () {
-		transform.position = waypoints [waypointIndex].transform.position;
-	}
-
-	void Update () {
-		Move ();
-	}
-
-	void Move()
-	{
-		transform.position = Vector2.MoveTowards (transform.position,
-												waypoints[waypointIndex].transform.position,
-												moveSpeed * Time.deltaTime);
-
-		if (transform.position == waypoints [waypointIndex].transform.position) {
-			waypointIndex += 1;
-		}
-				
-		if (waypointIndex == waypoints.Length)
-			waypointIndex = 0;
-	}
+    void Move()
+    {
+        // moves enemy in direction of next waypoint
+        transform.position = Vector2.MoveTowards(transform.position,
+                                                waypoints[waypointIndex].transform.position,
+                                                moveSpeed * Time.deltaTime);
+        // once waypoint is reached, changes waypoint to next one in array
+        if (transform.position == waypoints[waypointIndex].transform.position)
+        {
+            waypointIndex += 1;
+        }
+        // changes waypoint back to first in array
+        if (waypointIndex == waypoints.Length)
+        {
+            waypointIndex = 0;
+        }
+    }
 
 }
