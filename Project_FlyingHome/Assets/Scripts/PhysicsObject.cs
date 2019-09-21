d515341d5585573d9a40a2ad906e0de6c818ba58
Jeshua Johnson <jeshuajohnson12@gmail.com>
2019-09-21 18:37:46 -0500

    9/21/2019

--- Project_FlyingHome/Assets/Scripts/PhysicsObject.cs
+++ Project_FlyingHome/Assets/Scripts/PhysicsObject.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PhysicsObjectFH : MonoBehaviour
+public class PhysicsObject : MonoBehaviour
 {
 
     public float minGroundNormalY = .65f;
