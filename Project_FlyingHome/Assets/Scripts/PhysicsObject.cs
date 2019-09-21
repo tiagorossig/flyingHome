a572cd8a19812171414ec667940a7cba924c3404
Jeshua Johnson <jeshuajohnson12@gmail.com>
2019-09-21 15:39:28 -0500

    9/21/2019_0327p
    +added level layout
    + merger 1

--- Project_FlyingHome/Assets/Scripts/PhysicsObject.cs
+++ Project_FlyingHome/Assets/Scripts/PhysicsObject.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PhysicsObject : MonoBehaviour
+public class PhysicsObjectFH : MonoBehaviour
 {
 
     public float minGroundNormalY = .65f;
---
+++ Project_FlyingHome/Assets/Scripts/workshop.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class workshop : MonoBehaviour
+{
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = new Vextor3(1f, 0f, 0f);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void FixedUpdate()
+    {
+        //Runs after a specific amounnt of frames
+    }
+}
